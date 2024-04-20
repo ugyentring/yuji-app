82e75cd81bc75ef191a681d0452eeb34f55fad6a
Ugyen Tshering <tsheringugyen483@gmail.com>
2024-04-21 02:01:04 +0600

    created all the pages needed and done with the routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,28 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Service from "./pages/Service";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
-  )
-}
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/service" element={<Service />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
 
-export default App
+export default App;
---
+++ client/src/pages/Service.jsx
@@ -0,0 +1,9 @@
+const Service = () => {
+  return (
+    <div>
+      <h1>services</h1>
+    </div>
+  );
+};
+
+export default Service;
