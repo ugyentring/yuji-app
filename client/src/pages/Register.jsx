90c5228eac8df027cbab6b08fe8eb3896613c507
Ugyen Tshering <tsheringugyen483@gmail.com>
2024-04-21 03:57:13 +0600

    changes

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -7,7 +7,13 @@ const Register = () => {
         <div className="section-registration">
           <div className="container grid grid-two-cols">
             <div className="registration-image">
-              <img src={Yuji}/>
+              <img
+                style={{ borderRadius: "5px" }}
+                className="register-yuji"
+                src={Yuji}
+                width="400"
+                height="500"
+              />
             </div>
           </div>
         </div>
