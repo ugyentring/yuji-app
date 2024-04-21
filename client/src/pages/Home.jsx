4f34770c2760efb87f6967944203327f3e39e095
Ugyen Tshering <tsheringugyen483@gmail.com>
2024-04-21 20:32:22 +0600

    chhanges

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,24 @@
-
 const Home = () => {
   return (
     <>
-      <h1>Home</h1>
+      <main>
+        <section className="section-hero">
+          <div className="container grid grid-two-cols">
+            <div className="hero-content">
+              <p>A computer science student in GCIT</p>
+              <h1>Welcome to Ugyen Code</h1>
+              <p>
+                Greetings! I am Ugyen, a Computer Science enthusiast with a
+                focus on Blockchain technology. Explore my projects and insights
+                into programming languages and blockchain applications. Lets
+                embark on this coding journey together!
+              </p>
+            </div>
+          </div>
+        </section>
+      </main>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
