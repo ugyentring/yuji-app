b39309506c20c356c947035ae22118f7363b4796
Ugyen Tshering <tsheringugyen483@gmail.com>
2024-04-24 09:42:14 +0600

    changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,3 +1,6 @@
+import { Link } from "react-router-dom";
+import HomeImg from "../assets/home.png";
+
 const Home = () => {
   return (
     <>
@@ -13,6 +16,19 @@ const Home = () => {
                 into programming languages and blockchain applications. Lets
                 embark on this coding journey together!
               </p>
+              <div className="flex">
+                <Link to="/contact">
+                  <button className="bg-green-900">Connct Now</button>
+                </Link>
+
+                <Link to="/service">
+                  <button className="">Learn More</button>
+                </Link>
+              </div>
+            </div>
+
+            <div className="hero-image">
+              <img src={HomeImg} alt="Home image" width="400" height="500" />
             </div>
           </div>
         </section>
